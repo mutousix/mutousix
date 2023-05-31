5e9d212345fd0b336022113ea387cc760c8b76f9
mutousix <92351756+mutousix@users.noreply.github.com>
2023-05-31 16:47:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.6.0/node_exporter-1.6.0.linux-amd64.tar.gz
+sudo wget https://raw.githubusercontent.com/mutousix/mutousix/main/node_exporter-1.6.0.linux-amd64.tar.gz
 sudo tar -zxvf node_exporter-1.6.0.linux-amd64.tar.gz
 sudo cp node_exporter-1.6.0.linux-amd64/node_exporter /usr/local/bin/
-sudo wget https://raw.githubusercontent.com/mutousix/mutousix/main/node_exporter.service
 sudo cp -r node_exporter.service /etc/systemd/system/
 sudo useradd -M -s /bin/false node_exporter
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
